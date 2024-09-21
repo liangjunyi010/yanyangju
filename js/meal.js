c9b8161721c42728a2d61b2a7458b74ccb4f575e
LiangJunyi-010 <liangjunyi010@gmail.com>
2024-09-21 20:44:41 +0800

    [fix]:rotate navigation

--- js/meal.js
+++ js/meal.js
@@ -14,9 +14,10 @@ const removeActiveClasses = () => {
 };
 
 
+
 const open = document.getElementById("open");
 const close = document.getElementById("close");
-const container = document.querySelector(".container");
+const container = document.querySelector(".container2");
 
 open.addEventListener("click", () => container.classList.add("show-nav"));
 close.addEventListener("click", () => container.classList.remove("show-nav"));
