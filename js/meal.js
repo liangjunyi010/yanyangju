f56bff4c6f5be346a835a465438a03e7be6e946c
LiangJunyi-010 <liangjunyi010@gmail.com>
2024-09-21 21:45:57 +0800

    [feat]:auto scroll up to top after click navigation button

--- js/meal.js
+++ js/meal.js
@@ -19,9 +19,22 @@ const close = document.getElementById("close");
 const container = document.querySelector(".container2");
 const body = document.body;  // 选择 body 元素
 
+// 定义滚动到顶部的函数
+const scrollToTop = (callback) => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" // 平滑滚动到顶部
+  });
+
+  // 等待滚动完成后再执行旋转动画
+  setTimeout(callback, 200); // 500ms是滚动的时间，视具体情况可以调整
+};
+
 open.addEventListener("click", () => {
-  container.classList.add("show-nav");
-  body.classList.add("show-nav"); // 禁止滚动
+  scrollToTop(() => {
+    container.classList.add("show-nav");
+    body.classList.add("show-nav"); // 禁止滚动
+  });
 });
 
 close.addEventListener("click", () => {
